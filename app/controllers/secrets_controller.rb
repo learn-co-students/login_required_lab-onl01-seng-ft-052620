dfd9a1273a4f13357cd986d0ad2911b7507fcadb
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-08-11 14:17:24 -0600

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,10 @@
+class SecretsController < ApplicationController 
+    before_action :is_logged_in?, except: [:new]
+    def show 
+    end
+
+    private 
+    def is_logged_in? 
+        redirect_to '/login' unless session.include? :name
+    end
+end
